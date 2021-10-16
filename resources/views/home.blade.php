a73620ba27d7132ad3f83add2b15f5ea2e09804b
rook-ies <astinarsenal@gmail.com>
2021-10-16 18:10:38 +0700

    fixed card on view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,21 +22,19 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            @foreach($posts as $post)
             <div class="card">
-                <div class="card-header">{{ __('View Post') }}</div>
-
+                <div class="card-header">{{$post->title}}</div>
                 <div class="card-body">
-                    @foreach($posts as $post)
-                    <h2>{{$post->title}}</h2>
-                    <h6>{{$post->created_at}}</h6>
-                    <!-- <p>Published At: {{date('Y-m-d', strtotime($post->published_at))}}</p> -->
-                    <br>
                     <div>
                         {{$post->description}}
                     </div>
-                    @endforeach
+                    <br>
+                    <p> created at : {{$post->created_at}}</p>
                 </div>
             </div>
+            <br>
+            @endforeach
         </div>
     </div>
 </div>
